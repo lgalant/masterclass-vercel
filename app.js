3036022cda6335b505d428060e3bd9c57d6cc413
Leandro Galanterni <leandro@Leandros-MacBook-Pro-2.local>
2024-08-01 10:34:53 -0300

    agrego pg

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ import express from "express";
 const app = express()
 const PORT = 8000
 
+import {config} from './dbconfig.js'
+
+import pkg from 'pg'
+const {Client} = pkg;
 
 
 app.get('/', (req, res) => {
@@ -15,6 +19,15 @@ app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
 })
 
+app.get('/canciones',async (req, res) => {
+    const client = new Client(config);
+    await client.connect();
+    let result = await client.query("select * from public.canciones");
+    await client.end();
+    console.log(result.rows);
+    res.send(result.rows)
+  })
+  
 app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
   })
