f9baef2e9e806b06e97607c30e338bf7a4e2c14b
Leandro Galanterni <leandro@Leandros-MacBook-Pro-2.local>
2024-08-05 15:06:46 -0300

    agrego cors y apis nuevas

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ import {config} from './dbconfig.js'
 import pkg from 'pg'
 const {Client} = pkg;
 
+import cors from 'cors'
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -28,6 +30,28 @@ app.get('/canciones',async (req, res) => {
     res.send(result.rows)
   })
   
+
+app.get('/artistas',async (req, res) => {
+    const client = new Client(config);
+    await client.connect();
+    let result = await client.query("select * from public.artistas");
+    await client.end();
+    console.log(result.rows);
+    res.send(result.rows)
+  })
+
+
+  app.get('/artistas/:id/canciones',async (req, res) => {
+    const {id} = req.params;
+    const client = new Client(config);
+    await client.connect();
+    let result = await client.query("select c.* from public.canciones c, public.albumes a where c.album = a.id and artista=$1", [id]);
+    await client.end();
+    console.log(result.rows);
+    res.send(result.rows)
+  })
+
+
 app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
   })
